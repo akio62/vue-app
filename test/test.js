b5f07abc32b1a08f007d24316799f12a704dfa95
akio62 <akioyama61@gmail.com>
2020-04-16 01:21:17 +0900

    Add sample test.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+describe('Node exec test', function() {
+  it('sample test', function() {
+    assert.equal([1, 2, 3].indexOf(1), 0);
   });
 });
