9565b522ebb5024611f59010a5dfdd7c83a28574
akio62 <akioyama61@gmail.com>
2020-05-02 17:16:33 +0900

    Test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 describe('Node exec test', function() {
   it('sample test', function() {
-    assert.equal([1, 2, 3].indexOf(1), 0);
+    assert.equal([1, 2, 3].indexOf(4), 0);
   });
 });
